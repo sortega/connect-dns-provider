00c7c50bc20dce79071b5f4ad3f837671f8adf86
Sebastián Ortega <sortega@refeed.org>
2021-02-16 22:50:35 +0100

    First version of DnsConfigProvider

--- README.md
+++ README.md
@@ -2,3 +2,52 @@
 
 This is a small library to leverage DNS records of type SRV to avoid
 hardcoding lists of servers in Kafka Connect connector configurations
+
+## What are SRV DNS records?
+
+DNS most well known record types are A and CNAME, both of which allow us to
+resolve names like `example.com` into IPs like `93.184.216.34`. However,
+there are many more types of records like MX for managing email and SRV for
+service resolution.
+
+SRV records allow you to list which hostnames and ports are offering some
+service. This is specially handy to find several nodes of a cluster. For
+example Cassandra contact points, Kafka brokers or Zookeeper ensemble nodes.
+
+This is an example for a small Kafka cluster that you might define for your
+private DNS zone:
+
+```
+_kafka._tcp.default.svc.cluster.local.   86400 IN SRV 0 0 9093 broker-a4f3.default.svc.cluster.local.
+_kafka._tcp.default.svc.cluster.local.   86400 IN SRV 0 0 9093 broker-bf33.default.svc.cluster.local.
+_kafka._tcp.default.svc.cluster.local.   86400 IN SRV 0 0 9093 broker-z98a.default.svc.cluster.local.
+```
+
+You can check SRV records with `dig`:
+
+```bash
+> dig _kafka._tcp.default.svc.cluster.local srv +short
+0 0 9093 broker-a4f3.default.svc.cluster.local.
+0 0 9093 broker-bf33.default.svc.cluster.local.
+0 0 9093 broker-z98a.default.svc.cluster.local.
+```
+
+## How to use
+
+1. Add this library to Kafka connect classpath. TODO: how?
+   
+2. Configure `connect-distributed.properties`:
+
+   ```properties
+   config.providers=dns
+   config.providers.dns.class=org.refeed.kafka.config.DnsConfigProvider
+   ```
+3. Use `${dns:}` expressions in connector configurations. For example:
+
+   ```
+   "cassandra.contact.points": "${dns:_cassandra._tcp.cluster1.example.com}"
+   ```
+   
+## Testing
+
+TODO
---
+++ src/main/java/org/refeed/kafka/config/DnsConfigProvider.java
@@ -0,0 +1,93 @@
+package org.refeed.kafka.config;
+
+import org.apache.kafka.common.config.ConfigData;
+import org.apache.kafka.common.config.ConfigException;
+import org.apache.kafka.common.config.provider.ConfigProvider;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import java.util.*;
+
+/**
+ * An implementation of <code>ConfigProvider</code> that retrieves SRV DNS records.
+ *
+ * <p>
+ * How to configure in <code>connect-distributed.properties</code>:
+ * </p>
+ *
+ * <pre>
+ * config.providers=dns
+ * config.providers.dns.class=org.refeed.kafka.config.DnsConfigProvider
+ * </pre>
+ *
+ * <p>
+ * How to use in a connector configuration:
+ * </p>
+ *
+ * <pre>
+ * "cassandra.contact.points": "${dns:_cassandra._tcp.cluster1.example.com}"
+ * </pre>
+ */
+public class DnsConfigProvider implements ConfigProvider {
+    private final DirContext resolver;
+
+    public DnsConfigProvider() {
+        Hashtable<String, String> env = new Hashtable<>();
+        env.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+        env.put("java.naming.provider.url", "dns:");
+        try {
+            resolver = new InitialDirContext(env);
+        } catch (NamingException e) {
+            throw new ConfigException("Cannot initialize DNS lookup", e);
+        }
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+    }
+
+    @Override
+    public ConfigData get(String unusedPath) {
+        return new ConfigData(Collections.emptyMap());
+    }
+
+    @Override
+    public ConfigData get(String unusedPath, Set<String> names) {
+        Map<String,String> resolvedNames = new HashMap<>();
+        for (String name: names) {
+            resolvedNames.put(name, resolve(name));
+        }
+        return new ConfigData(resolvedNames);
+    }
+
+    private String resolve(String name) {
+        final NamingEnumeration<?> records;
+        try {
+            records = resolver.getAttributes(name, new String[]{"SRV"}).get("srv").getAll();
+        } catch (NamingException e) {
+            throw new ConfigException("Cannot resolve SRV record for " + name, e);
+        }
+        List<String> servers = new LinkedList<>();
+        while (records.hasMoreElements()) {
+            String record = (String) records.nextElement();
+            servers.add(parseSrvRecord(record));
+        }
+        return String.join(",", servers);
+    }
+
+    private String parseSrvRecord(String record) {
+        String[] parts = record.split("\\s+");
+        if (parts.length < 4) {
+            throw new ConfigException("Malformed SRV record: '" + record +"'");
+        }
+        long port = Long.parseLong(parts[2]);
+        String host = parts[3].substring(0, parts[3].length() - 1);
+        return host + ":" + port;
+    }
+
+    @Override
+    public void close() {
+    }
+}
